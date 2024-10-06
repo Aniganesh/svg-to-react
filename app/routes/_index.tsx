efef64b335737b52ecf49839c2edbc3641029882
aniganesh <aniganesh741@gmail.com>
2024-10-07 01:17:11 +0530

    wip basic proof done

---
+++ app/routes/_index.tsx
@@ -0,0 +1,63 @@
+import { type MetaFunction } from "@remix-run/node";
+import { convertSvgToReact } from "../script";
+import { BlobReader, ZipReader, TextWriter, BlobWriter, ZipWriter, TextReader } from "@zip.js/zip.js";
+
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "SVG to React" },
+    { name: "SVG to React", content: "Convert your svg files to react components" },
+  ];
+};
+
+export default function Index() {
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const file = (formData.get("file") as File | null);
+    if (!file) {
+      return;
+    }
+    if (file.type === "application/zip") {
+      const zipFileReader = new BlobReader(file);
+      const files = await (await new ZipReader(zipFileReader)).getEntries();
+      const res = await Promise.all(files.map(async (file, index) => {
+        if (!file.getData) return '';
+        const textContentWriter = new TextWriter();
+        const content = await file.getData(textContentWriter);
+        const svgContent = convertSvgToReact(content, "Test" + index);
+        return svgContent;
+      }));
+
+      const zipFileWriter = new BlobWriter();
+      const zipWriter = new ZipWriter(zipFileWriter);
+
+      Array.from(res.values()).filter((v) => !!v).map((fileContent) => zipWriter.add("Test.tsx", new TextReader(fileContent)));
+
+      const url = URL.createObjectURL(await zipWriter.close());
+      const downloadLink = document.createElement("a");
+      downloadLink.href = url;
+      downloadLink.download = "Test.zip";
+      downloadLink.click();
+      return;
+    }
+    const fileContent = await file.text();
+    const svgContent = convertSvgToReact(fileContent, "Test");
+    const blob = new Blob([svgContent], { type: "text/plain", });
+    const url = URL.createObjectURL(blob);
+    const downloadLink = document.createElement("a");
+    downloadLink.href = url;
+    downloadLink.download = "Test.tsx";
+    downloadLink.click();
+  };
+
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <form onSubmit={handleSubmit}>
+        <input type="file" name="file" id="file" accept="image/svg, application/zip" multiple />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
---
+++ app/script.ts
@@ -0,0 +1,366 @@
+const allSVGAttributes = [
+  "accent-height",
+  "accumulate",
+  "additive",
+  "alignment-baseline",
+  "alphabetic",
+  "amplitude",
+  "arabic-form",
+  "ascent",
+  "attributeName",
+  "attributeType",
+  "azimuth",
+  "baseFrequency",
+  "baseline-shift",
+  "baseProfile",
+  "bbox",
+  "begin",
+  "bias",
+  "by",
+  "calcMode",
+  "cap-height",
+  "class",
+  "clip",
+  "clipPathUnits",
+  "clip-path",
+  "clip-rule",
+  "color",
+  "color-interpolation",
+  "color-interpolation-filters",
+  "color-rendering",
+  "crossorigin",
+  "cursor",
+  "cx",
+  "cy",
+  "d",
+  "decelerate",
+  "descent",
+  "diffuseConstant",
+  "direction",
+  "display",
+  "divisor",
+  "dominant-baseline",
+  "dur",
+  "dx",
+  "dy",
+  "edgeMode",
+  "elevation",
+  "end",
+  "exponent",
+  "fill",
+  "fill-opacity",
+  "fill-rule",
+  "filter",
+  "filterUnits",
+  "flood-color",
+  "flood-opacity",
+  "font-family",
+  "font-size",
+  "font-size-adjust",
+  "font-stretch",
+  "font-style",
+  "font-variant",
+  "font-weight",
+  "format",
+  "from",
+  "fr",
+  "fx",
+  "fy",
+  "g1",
+  "g2",
+  "glyph-name",
+  "glyph-orientation-horizontal",
+  "glyph-orientation-vertical",
+  "glyphRef",
+  "gradientTransform",
+  "gradientUnits",
+  "hanging",
+  "height",
+  "href",
+  "hreflang",
+  "horiz-adv-x",
+  "horiz-origin-x",
+  "id",
+  "ideographic",
+  "image-rendering",
+  "in",
+  "in2",
+  "intercept",
+  "k",
+  "k1",
+  "k2",
+  "k3",
+  "k4",
+  "kernelMatrix",
+  "kernelUnitLength",
+  "keyPoints",
+  "keySplines",
+  "keyTimes",
+  "lang",
+  "lengthAdjust",
+  "letter-spacing",
+  "lighting-color",
+  "limitingConeAngle",
+  "local",
+  "marker-end",
+  "marker-mid",
+  "marker-start",
+  "markerHeight",
+  "markerUnits",
+  "markerWidth",
+  "mask",
+  "maskContentUnits",
+  "maskUnits",
+  "mathematical",
+  "max",
+  "media",
+  "method",
+  "min",
+  "mode",
+  "name",
+  "numOctaves",
+  "offset",
+  "opacity",
+  "operator",
+  "order",
+  "orient",
+  "orientation",
+  "origin",
+  "overflow",
+  "overline-position",
+  "overline-thickness",
+  "panose-1",
+  "paint-order",
+  "pathLength",
+  "patternContentUnits",
+  "patternTransform",
+  "patternUnits",
+  "ping",
+  "pointer-events",
+  "points",
+  "pointsAtX",
+  "pointsAtY",
+  "pointsAtZ",
+  "preserveAlpha",
+  "preserveAspectRatio",
+  "primitiveUnits",
+  "r",
+  "radius",
+  "referrerPolicy",
+  "refX",
+  "refY",
+  "rel",
+  "rendering-intent",
+  "repeatCount",
+  "repeatDur",
+  "requiredExtensions",
+  "requiredFeatures",
+  "restart",
+  "result",
+  "rotate",
+  "rx",
+  "ry",
+  "scale",
+  "seed",
+  "shape-rendering",
+  "slope",
+  "spacing",
+  "specularConstant",
+  "specularExponent",
+  "speed",
+  "spreadMethod",
+  "startOffset",
+  "stdDeviation",
+  "stemh",
+  "stemv",
+  "stitchTiles",
+  "stop-color",
+  "stop-opacity",
+  "strikethrough-position",
+  "strikethrough-thickness",
+  "string",
+  "stroke",
+  "stroke-dasharray",
+  "stroke-dashoffset",
+  "stroke-linecap",
+  "stroke-linejoin",
+  "stroke-miterlimit",
+  "stroke-opacity",
+  "stroke-width",
+  "style",
+  "surfaceScale",
+  "systemLanguage",
+  "tabindex",
+  "tableValues",
+  "target",
+  "targetX",
+  "targetY",
+  "text-anchor",
+  "text-decoration",
+  "text-rendering",
+  "textLength",
+  "to",
+  "transform",
+  "transform-origin",
+  "type",
+  "u1",
+  "u2",
+  "underline-position",
+  "underline-thickness",
+  "unicode",
+  "unicode-bidi",
+  "unicode-range",
+  "units-per-em",
+  "v-alphabetic",
+  "v-hanging",
+  "v-ideographic",
+  "v-mathematical",
+  "values",
+  "vector-effect",
+  "version",
+  "vert-adv-y",
+  "vert-origin-x",
+  "vert-origin-y",
+  "viewBox",
+  "visibility",
+  "width",
+  "widths",
+  "word-spacing",
+  "writing-mode",
+  "x",
+  "x-height",
+  "x1",
+  "x2",
+  "xChannelSelector",
+  "xlink:actuate",
+  "xlink:arcrole",
+  "xlink:href Deprecated",
+  "xlink:role",
+  "xlink:show",
+  "xlink:title",
+  "xlink:type",
+  "xml:lang",
+  "xml:space",
+  "y",
+  "y1",
+  "y2",
+  "yChannelSelector",
+  "z",
+  "zoomAndPan",
+];
+
+function toStartCase(str: string) {
+  const words = str.split("-");
+  const res: string[] = [];
+  words.forEach((word) => {
+    word = word.replace(/[^a-zA-Z]/g, "");
+    res.push(word.charAt(0).toUpperCase() + word.slice(1));
+  });
+  return res.join("");
+}
+
+function toCamelCase(str: string) {
+  const words = str.split("-");
+  const res: string[] = [];
+  words.slice(1).forEach((word) => {
+    word = word.replace(/[^a-zA-Z]/g, "");
+    res.push(word.charAt(0).toUpperCase() + word.slice(1));
+  });
+  return [words[0]].concat(res).join("");
+}
+
+interface TemplateOptions {
+  addDefaultExport?: boolean;
+  language: "ts" | "js";
+}
+
+const defaultTemplateOptions: TemplateOptions = {
+  addDefaultExport: true,
+  language: "ts",
+};
+
+const reactTSImport = "import React, { FC } from 'react';";
+const reactJSImport = "import React from 'react';";
+
+const reactTSInterfaceWithoutExtend = "interface %name%Props { }";
+// TODO: Add support for using interface with extend
+const reactTSInterfaceWithExtend = "interface %name%Props extends %extend% {}";
+
+const defaultExport = "export default %name%;";
+
+const namedExportTS = `export const %name%: FC<%name%Props> = (props) => {
+	return %componentContent%;
+};
+`;
+const namedExportJS = `export const %name% = (props) => {
+	return %componentContent%;
+};
+`;
+
+const groupedByLanguage = {
+  js: {
+    import: reactJSImport,
+    namedExport: namedExportJS,
+    defaultExport,
+  } as const,
+  ts: {
+    import: reactTSImport,
+    namedExport: namedExportTS,
+    interfaceWithoutExtend: reactTSInterfaceWithoutExtend,
+    interfaceWithExtend: reactTSInterfaceWithExtend,
+    defaultExport,
+  } as const,
+} as const;
+
+const XMLDeclarationRegex = /<\?xml\s+.*\?>/;
+const HTMLCommentRegex = /<!--[\s\S]*?-->/;
+
+const getReactTemplate = (options: TemplateOptions) => {
+  const template: string[] = [];
+  const setToUse = groupedByLanguage[options.language];
+
+  template.push(setToUse.import);
+
+  if (options.language === "ts") {
+    template.push(
+      (setToUse as (typeof groupedByLanguage)["ts"]).interfaceWithoutExtend
+    );
+  }
+
+  template.push(setToUse.namedExport);
+
+  if (options.addDefaultExport) {
+    template.push(setToUse.defaultExport);
+  }
+
+  return template.join("\n");
+};
+
+const attributesWithHyphen = allSVGAttributes.filter((attribute) =>
+  attribute.includes("-")
+);
+
+export const convertSvgToReact = (
+  svgContent: string,
+  name: string,
+  templateOptions: TemplateOptions = defaultTemplateOptions
+) => {
+  // replace each of the disallowed attributes with their camelcase equivalents.
+  attributesWithHyphen.forEach((attribute) => {
+    if (svgContent.includes(attribute)) {
+      svgContent = svgContent.replace(
+        new RegExp(`${attribute}=`, "g"),
+        toCamelCase(attribute) + "="
+      );
+    }
+  });
+  let fileContent = getReactTemplate(templateOptions).replace(
+    /%name%/g,
+    toStartCase(name)
+  );
+  fileContent = fileContent.replace(/%componentContent%/g, svgContent);
+  fileContent = fileContent.replace(XMLDeclarationRegex, "");
+  fileContent = fileContent.replace(HTMLCommentRegex, "");
+  fileContent = fileContent.replace(/return\s*\n/g, "return");
+  return fileContent;
+};
